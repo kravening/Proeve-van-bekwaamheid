96532f372f9ac97c3daba8e6e9b92465bd12b57a
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-04-17 23:01:57 +0200

    setup a simple playfield

---
+++ The Johnny ARcade/Assets/SquirrelCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelCollision : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collider)
+    {
+        gameObject.GetComponent<Animation>().clip.SampleAnimation(this.gameObject,0);
+        AddScore();
+    }
+
+    public void AddScore()
+    {
+        
+    }
+}
