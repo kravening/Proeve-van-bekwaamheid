851752a283238f2dbdd2cbc83609ef2b75bfc9ac
MrElektronix <milovanpelt@gmail.com>
2019-04-18 09:23:36 +0200

    Made a shooting script for the prototype

---
+++ The Johnny ARcade/Assets/_Misc/Scripts/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject projectile;
+    private GameObject _clone;
+
+    private void Update()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            _clone = Instantiate(projectile, transform.position, transform.rotation);
+            _clone.GetComponent<Rigidbody>().AddForce(_clone.transform.forward * 100);
+        } 
+    }
+    
+}
